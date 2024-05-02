e51e6c4d20a49883aa4704052e6a8505a14a5eb1
Blander24 <benlands101@gmail.com>
2024-05-02 19:37:01 -0400

    version 0.1.0

---
+++ fruit-trees-template-1.20.1/src/main/java/net/benji/fruittrees/FruitTreesDataGenerator.java
@@ -0,0 +1,11 @@
+package net.benji.fruittrees;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class FruitTreesDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
